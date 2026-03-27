d3d2c9b5134102a901ad67759f0c9ba838ba3c23
Hung-Yang-Shih <d11416204@gs.takming.edu.tw>
2026-03-27 11:40:04 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Test
+{
+
+    public int Mid;
+    public int Final;
+
+    public Test(int m, int f)
+    {
+        this.Mid= Mid; 
+        this.Final = Final;
+    }
+    
+    public void SetMid(int value)
+    {
+        this.Mid = Mid;
+    }
+
+    public void SetFinal(int value)
+    {
+        this.Final = Final;
+    }
+
+    public int GetMid()
+    {
+        return this.Mid;
+    }
+
+    public int GetFinal()
+    {
+        return this.Final;
+    }
+    
+    public double GetAvg()
+    {
+        return (Mid+Final)/2.0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _0327_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1 = new Test(45, 85);
+            Test std2 = new Test(68, 84);
+
+            std1.SetMid(88);
+            std1.SetFinal(82);
+
+            Console.WriteLine("1: "+std1.Mid.ToString());
+            Console.WriteLine("2: "+std2.Final.ToString());
+        }
+    }
+}
